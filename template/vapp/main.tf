95c270ea2c8b2d96478710ba584df315da9074ea
rohitg <rohitg@jumphost.icplab.com>
2018-10-11 17:12:55 +1300

    ip instead

---
+++ template/vapp/main.tf
@@ -0,0 +1,93 @@
+# This is a terraform generated template generated from blueprint89
+
+##############################################################
+# Keys - CAMC (public/private) & optional User Key (public)
+##############################################################
+variable "allow_unverified_ssl" {
+  description = "Communication with vsphere server with self signed certificate"
+  default     = "true"
+}
+
+##############################################################
+# Define the vsphere provider
+##############################################################
+
+
+provider "camc" {
+  version = "~> 0.2"
+}
+
+##############################################################
+# Define pattern variables
+##############################################################
+
+##############################################################
+# Vsphere data for provider
+##############################################################
+
+
+#########################################################
+##### Resource : vm
+#########################################################
+
+variable "vcd_user_name" {
+  description = "User name for VCD login"
+}
+
+variable "vcd_user_password" {
+  description = "Password for VCD login"
+}
+
+variable "vcd_org_name" {
+  description = "Org name"
+  default = "tstcs1"
+}
+
+variable "vcd_host_url" {
+  description = "VCD host url"
+  default     = "https://103.5.83.14/api"
+}
+
+variable "vcd_vdc_name" {
+  description = "VDC name"
+  default     = "TSTCS1-PAYG"
+}
+
+variable "vcd_vapp_name" {
+  description = "Vapp name"
+}
+
+variable "vcd_vm_name" {
+  description = "VM name"
+}
+
+variable "vcd_catalog_name" {
+  description = "catalog name"
+  default = "Public IAAS"
+}
+
+variable "vcd_vm_template_name" {
+  description = "template name"
+  default = "W2016_STD_Full_Gold_201710"
+}
+
+variable "vcd_network_name" {
+  description = "Network name"
+  default = "TWG-ICP"
+}
+
+# VCD vm provision
+provider "vcd" {
+  allow_unverified_ssl = "${var.allow_unverified_ssl}"
+  user = "${var.vcd_user_name}"
+  password = "${var.vcd_user_password}"
+  org = "${var.vcd_org_name}"
+  url = "${var.vcd_host_url}"
+  vdc = "${var.vcd_vdc_name}"
+}
+
+
+resource "vcd_vapp" "${var.vcd_vapp_name}" {
+  name  = "${var.vcd_vapp_name}"
+
+}
